307e7090f5a3e6d8ae7bf9ebf88a2bb5e20623a8
Margots Kapacs <makapacs@hotmail.com>
2013-09-02 11:03:35 -0500

    Fixing IE. removed responsive

--- sites/all/themes/bazar/template.php
+++ sites/all/themes/bazar/template.php
@@ -942,7 +942,7 @@ function bazar_preprocess_html(&$vars, $hook) {
             $bazarJsButtom['jquery-mousewheel']['external']=false;
 
 //            $bazarCssBodyTag = 'no_js responsive';
-            $bazarCssBodyTag = 'page page-child page-template-default no_js stretched responsive';
+            $bazarCssBodyTag = 'page page-child page-template-default no_js stretched';
 
             $bazarJsConfigButtom = 'var thickboxL10n = {"next":"Next >","prev":"< Prev","image":"Image","of":"of","close":"Close","noiframes":"This feature requires inline frames. You have iframes disabled or your browser does not support them.","loadingAnimation":" ","closeImage":" "};'."\n"
                 . 'var l10n_handler = {"map_close":"[x] Close","map_open":"[x] Open"};'."\n"
